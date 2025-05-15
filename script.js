54085ccf390cd0557345e6c9fa23bc450014d836
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-05-15 23:20:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
 //your JS code here. If required.
+const btn = document.getElementById('btn');
+btn.addEventListener('click' , async ()=>{
+	try {
+		let res = await fetch(url);
+		let data = await res.json();
+		console.log(data);
+		
+		let output = document.getElementById('output');
+		output.innerText = `${data}`;
+	} catch (error) {
+		console.log(error);
+	}
+})
